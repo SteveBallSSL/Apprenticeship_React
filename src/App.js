f60729092f93fdcd40b9455b19dbb93af1b11493
MargateSteve <MargateSteve@users.noreply.github.com>
2023-10-16 16:01:08 +0100

    SRB: use state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './css/App.css';
 import allBlogs from './json/blogsData.json';
 import BlogList from './components/BlogList';
+import { useState } from 'react';
+
 
 function App() {
 
+  const [blogs, setBlogs] = useState(allBlogs);
+
   function deleteBlog(id) {
-    allBlogs = allBlogs.filter(item => item.id !== id)
+    let filteredBlogs = blogs.filter((blog) => blog.id != id);
+    setBlogs(filteredBlogs);
   }
+
 
   return (
     <div>
-      <BlogList blogs={allBlogs} deleteBlog={deleteBlog}/>
+      <BlogList blogs={blogs} deleteBlog={deleteBlog}/>
     </div>
 
   )
