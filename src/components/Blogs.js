78c7f76b9d200fa43e9954359d4b76d91ecd1f07
MargateSteve <MargateSteve@users.noreply.github.com>
2023-10-17 14:41:02 +0100

    SRB: Error handling

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,18 +1,33 @@
 import BlogList from './BlogList';
+import Spinner from './Spinner';
 import { useEffect, useState } from 'react';
 
 function Blogs() {
     const [blogs, setBlogs] = useState(null);
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
 
     useEffect(() => {
         fetch('http://localhost:5000/blogs')
             .then(res => {
+                // For 200 error
+                if (!res.ok) {
+                    throw Error('Could not get data for that resource.');
+                }
+
                 return res.json();
             }).then(data => {
-                setBlogs(data);
+                setTimeout(() => {
+                    setBlogs(data);
+                    setError(null);
+                }, 2000);
+            }).catch(err => {
+                setIsLoading(false);
+                setError(err.message);
             })
     }, []);
-    
+
 
     function deleteBlog(id) {
         const newBlogs = blogs.filter(item => item.id !== id);
@@ -21,10 +36,13 @@ function Blogs() {
 
     // can use fragments instead of div to stop REACT complaining <> </>
     return (
-        <> 
-            {blogs && <BlogList blogs={blogs} deleteBlog={deleteBlog} /> }
-        </>
+        <div>
+            {error && <div>{error}</div>}
+            {isLoading && <Spinner />}
+            {blogs && <BlogList blogs={blogs} deleteBlog={deleteBlog} />}
+        </div>
     );
+
 }
 
 export default Blogs;
---
+++ src/components/Spinner.js
@@ -0,0 +1,6 @@
+import classes from "../css/spinner.css"
+
+function Spinner(props){
+    return <div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>;
+}
+export default Spinner;
