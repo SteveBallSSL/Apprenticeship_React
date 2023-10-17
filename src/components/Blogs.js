81a1344f6e0ca673154baace8d87bed757dc04a5
MargateSteve <MargateSteve@users.noreply.github.com>
2023-10-17 11:45:58 +0100

    SRB: Ajax call with useEffect hook to stop infinite calls

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,16 +1,30 @@
-import allBlogs from '../json/blogData.json';
 import BlogList from './BlogList';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function Blogs() {
-    const [blogs, setBlogs] = useState(allBlogs);
+    const [blogs, setBlogs] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/blogs')
+            .then(res => {
+                return res.json();
+            }).then(data => {
+                setBlogs(data);
+            })
+    }, []);
+    
 
     function deleteBlog(id) {
         const newBlogs = blogs.filter(item => item.id !== id);
         setBlogs(newBlogs);
     }
 
-    return ( <BlogList blogs={blogs} deleteBlog={deleteBlog} />);
+    // can use fragments instead of div to stop REACT complaining <> </>
+    return (
+        <> 
+            {blogs && <BlogList blogs={blogs} deleteBlog={deleteBlog} /> }
+        </>
+    );
 }
 
 export default Blogs;
